ede04ca2c8cff0241f20e6a674eea5a981fec7cb
kalelgwapo <kalelreyes@gmail.com>
2016-11-19 00:44:55 +0800

    Implemented Get of a user's timeline using twitter's default screen name

--- PopularTweets/PopularTweets/Services/TwitterService.cs
+++ PopularTweets/PopularTweets/Services/TwitterService.cs
@@ -11,8 +11,23 @@ namespace PopularTweets.Services
     public class TwitterService
     {
         public bool GetTwitterTimelineViaScreenName(string screenName) {
-            if(string.IsNullOrEmpty(TwitterSingleton.Instance.accessToken) && string.IsNullOrEmpty(TwitterSingleton.Instance.tokenType))
+            if (string.IsNullOrEmpty(TwitterSingleton.Instance.accessToken) || string.IsNullOrEmpty(TwitterSingleton.Instance.tokenType))
                 TwitterSingleton.Instance.Authenticate();
+
+            HttpWebRequest timeLineRequest = (HttpWebRequest)WebRequest.Create("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2");
+            var timelineHeaderFormat = "{0} {1}";
+            timeLineRequest.Headers.Add("Authorization", string.Format(timelineHeaderFormat, TwitterSingleton.Instance.tokenType, TwitterSingleton.Instance.accessToken));
+            timeLineRequest.Method = "Get";
+            WebResponse timeLineResponse = timeLineRequest.GetResponse();
+            var timeLineJson = string.Empty;
+            using (timeLineResponse)
+            {
+                using (var reader = new StreamReader(timeLineResponse.GetResponseStream()))
+                {
+                    timeLineJson = reader.ReadToEnd();
+                }
+            }
+                
             return true;
         }
     }
